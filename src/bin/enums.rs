a948bbcd3e14d746b20239c11d4d4ec3696372f0
ChristianPG <christianpg813@gmail.com>
2023-11-16 16:47:08 -0500

    Finished the enums chapter

--- src/bin/enums.rs
+++ src/bin/enums.rs
@@ -3,28 +3,50 @@ enum IpAddressKind {
     V6(String),
 }
 
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
 }
 
-impl Message {
-    fn call(&self) {
-        // method body would be defined here
-    }
+// #[derive(Debug)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
 }
 
 fn main() {
+    // Example of Enums using IP addresses
     let home = IpAddressKind::V4(String::from("127.0.0.1"));
     let loopback = IpAddressKind::V6(String::from("::1"));
 
-    let m = Message::Write(String::from("hello"));
-    m.call();
-
+    // Examples of the Option built-in enum
     let some_number = Some(5);
     let some_char = Some('e');
-
     let absent_number: Option<i32> = None;
+    
+
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky Penny!!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
+}
+
+fn verify_quarter(coin: Coin) {
+    if let Coin::Quarter(state) = coin {
+        println!("This is a shiny quarter from {:?}", state);
+    }
 }
